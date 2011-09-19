d857d936bf9bb6fd6e537ee31ace9767504f61e7
nerduino <nerduino@gmail.com>
2011-09-19 15:44:37 +0800

    Updated

--- Utils/Logger.cs
+++ Utils/Logger.cs
@@ -2,29 +2,24 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using Microsoft.SPOT;
-using NetMf.CommonExtensions;
+
 
 namespace Utils
 {
     public class Logger
     {
-        private static string LOGMESSAGE = "{0} {1} - {2}";
         private static StreamWriter logWriter = null;
 
         public static LogLevel Threshold { get; set; }
 
-        public static void Log(LogLevel level, string message, params object[] args)
-        {
-            Log(level, StringUtility.Format(message, args));
-        }
-        
+
         public static void Log(LogLevel level, string message)
         {
             if (logWriter == null)
             {
                 initLogWriter();
             }
-            string logMessage = StringUtility.Format(LOGMESSAGE, DateTime.Now.ToString(), level.ToString(), message);
+            string logMessage = DateTime.Now.ToString() +" "+level.ToString()+" "+message;
 
             if (Threshold <= level)
             {
@@ -32,7 +27,7 @@ namespace Utils
             }
             if (level == LogLevel.DEBUG)
             {
-                ConsoleDebug(message);
+                ConsoleDebug(logMessage);
             }
         }
 
@@ -48,7 +43,8 @@ namespace Utils
             }
         }
 
-        private static void Log(string message) {
+        private static void Log(string message)
+        {
             if (logWriter != null)
             {
                 try
@@ -62,40 +58,29 @@ namespace Utils
             }
         }
 
-        
+
         [Conditional("DEBUG")]
         private static void ConsoleDebug(string message)
         {
             Microsoft.SPOT.Debug.Print(message);
         }
 
-        
-        public static void Debug(string message) {
+
+        public static void Debug(string message)
+        {
             Log(LogLevel.DEBUG, message);
         }
 
-        public static void Debug(string message, params object[] args)
-        {
-            Log(LogLevel.DEBUG, message, args);
-        }
-
+       
         public static void Info(string message)
         {
             Log(LogLevel.INFO, message);
         }
-        public static void Info(string message, params object[] args)
-        {
-            Log(LogLevel.INFO, message, args);
-        }
+       
         public static void Error(string message)
         {
             Log(LogLevel.ERROR, message);
         }
-        public static void Error(string message, params object[] args)
-        {
-            Log(LogLevel.ERROR, message, args);
-        }
-
     }
 
     public enum LogLevel : byte
